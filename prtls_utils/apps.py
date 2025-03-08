b9e3969976e3189761fbe9a260ef26dde59db34a
protellus <129015214+protellus@users.noreply.github.com>
2025-03-08 11:38:28 -0500

    Correcting name

--- prtls_utils/apps.py
+++ prtls_utils/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
+# This is the configuration class for the prtls_utils app.
 
-class PrtlUtilsConfig(AppConfig):
+class PrtlsUtilsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prtls_utils'
