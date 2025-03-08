54ba88d30e3286258e43155fe63b64048d739b98
protellus <129015214+protellus@users.noreply.github.com>
2025-03-08 06:41:39 -0500

    Pinned all dependency revisions to the cba project

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="putils",  
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "django==3.2",
+        "asgiref==3.7.2",
+        "sqlparse==0.4.4",
+        "tzdata==2025.1"
     ],
     classifiers=[
         "Framework :: Django",
