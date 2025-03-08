c00da9d48dc9d6b82f638d92407675ce19fd79fc
protellus <129015214+protellus@users.noreply.github.com>
2025-03-08 05:40:04 -0500

    Matching versions to cba project

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="putils",  
-    version="0.1.0",
+    #version="0.1.0",
+    version="0.1.1", # Matching the parent project version
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "django>=3.2",
+        "django==3.2",
     ],
     classifiers=[
         "Framework :: Django",
