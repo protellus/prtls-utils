b07369acd8c3b06d3bd3f62701da6396b899304c
protellus <129015214+protellus@users.noreply.github.com>
2025-03-08 12:16:52 -0500

    Rebuild environment for name change

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="putils",  
-    version="0.1.4",
+    name="prtls_utils",  
+    version="0.1.5",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "django==3.2",
         "asgiref==3.7.2",
         "sqlparse==0.4.4",
-        "tzdata==2025.1"
-    ],
-    classifiers=[
-        "Framework :: Django",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "pytz==2023.3"
     ],
 )
